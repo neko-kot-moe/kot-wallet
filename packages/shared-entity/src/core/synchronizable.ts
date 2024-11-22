b6e362973425271d2006be6950c3efda8168fbcb
Neko Kot Moe <neko@kot.moe>
2024-11-22 23:45:08 +0400

    Add deleted flag

--- packages/shared-entity/src/core/synchronizable.ts
+++ packages/shared-entity/src/core/synchronizable.ts
@@ -3,6 +3,8 @@ import {z} from 'zod'
 export const synchronizableSchema = z.object({
   id: z.string().uuid(),
 
+  deleted: z.boolean(),
+
   createdAt: z.number().int().positive(),
   updatedAt: z.number().int().positive(),
 })
